b601dd8935b185e925299dc7cbcff5e3ffd8ef58
Rageshmcs <ragesh.mcs@gmail.com>
2020-10-15 00:15:49 +0530

    URL change

--- modules/authentication/services.js
+++ modules/authentication/services.js
@@ -6,10 +6,12 @@ angular.module('Authentication')
     ['Base64', '$http', '$cookieStore', '$rootScope', '$timeout',
     function (Base64, $http, $cookieStore, $rootScope, $timeout) {
         var service = {};
+        $rootscope.postsUrl = "https://react-or-no-react.herokuapp.com/"
+        $rootScope.serverUrl = "https://artful-iudex.herokuapp.com/"; // Another URL "https://my-json-server.typicode.com/artfuldev/json-db-data/"
 
         service.Login = function (username, password, callback) {
 
-            $http.post('http://localhost:3000/api/v1/users/search', { username: username, password: password })
+            $http.post($rootscope.postsUrl + 'api/v1/users/search', { username: username, password: password })
                .success(function (response) {
                    callback(response);
                });
